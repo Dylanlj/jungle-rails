a5ec22972586b7837c1d2e00735c63aae45df1b1
Dylanlj <dylanl-j@hotmail.ca>
2018-04-03 19:04:55 +0000

    finished testing for product link

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,30 +1,33 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
+RSpec.feature "ProductDetails", type: :feature, js: true do
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
-      @category.products.create!(
+
+    @product =  @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
-    end
+
   end
 
-  scenario "They see all products" do
+  scenario "product link takes them to the products show page" do
     # ACT
+
+    # binding.pry
     visit root_path
+    # puts @product.name
+    click_on @product.name
+
+    # VERIFY
+    expect(page).to have_content(@product.name) && have_css('section.products-show')
 
     # DEBUG
     save_screenshot
-
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
   end
 end
+
